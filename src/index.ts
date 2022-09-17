babfa86158ab03e8a1ffdb2bd11afddaa50cc90a
Ahmed-Abdella <abdellaa921@gmail.com>
2022-09-17 04:31:38 +0200

    building the sort function with inteface to check if the collectio sortable

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-console.log("hi");
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
 
-const a = [];
+const numbersCollection = new NumbersCollection([10000, 3, -5, 0]);
+
+const sorter = new Sorter(numbersCollection);
+sorter.sort();
+
+console.log(sorter.collection);
