34bd9ea4d1232946089c03f99cefddd564b290d3
Ahmed-Abdella <abdellaa921@gmail.com>
2022-09-21 20:13:39 +0200

    finish design patterns 1 -using classes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
-import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LInkedList";
 
 const numbersCollection = new NumbersCollection([10000, 3, -5, 0]);
+const characterCollection = new CharactersCollection("klangnomMMMad");
+const linkedList = new LinkedList();
 
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+linkedList.add(5);
+linkedList.add(-1000);
+linkedList.add(300);
+linkedList.add(10);
 
-console.log(sorter.collection);
+numbersCollection.sort();
+characterCollection.sort();
+linkedList.sort();
+
+console.log(characterCollection.data);
+console.log(numbersCollection.data);
+linkedList.print();
